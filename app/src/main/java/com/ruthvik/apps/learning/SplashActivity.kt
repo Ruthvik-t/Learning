2910c9c313368271bd1b16c3e1edc8dc177d33a3
Ruthvik <Ruthvik.Thiriveedhi@tesco.com>
2021-09-01 18:19:33 +0530

    added a splash screen.

---
+++ app/src/main/java/com/ruthvik/apps/learning/SplashActivity.kt
@@ -0,0 +1,11 @@
+package com.ruthvik.apps.learning
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
